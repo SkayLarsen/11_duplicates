fbbd629af4ab491dd5e8780ae23a3793be5e643f
SkayLarsen <SkayLarsen@gmail.com>
2017-01-05 19:41:14 +0300

    задача #11

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Скрипт принимает на вход папку, просматривает все файлы в ней (и во всех подпапках) и сообщает, если находит дубликаты. 
+Дубликаты – это два файла с одинаковым именем и размером.
+
+Скрипт требует для своей работы установленный интерпретатор Python версии 3.5
+
+Запуск на Linux:
+
+```#!bash
+$ python duplicates.py <path to folder>
+```
+Запуск на Windows происходит аналогично.
+
+Пример вывода скрипта:
+
+```
+Дубликаты:      ./.git/logs/HEAD и ./.git/logs/refs/remotes/origin/HEAD
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,37 @@
+import os.path
+import sys
+from collections import defaultdict
+from itertools import combinations
+
+
+def inspect_folder(folder):
+    if not os.path.isdir(folder):
+        return None
+    filepaths = defaultdict(list)
+    for root, dirs, files in os.walk(folder):
+        for filename in files:
+            fullpath = os.path.join(root, filename)
+            filepaths[filename].append(fullpath)
+    return {name: paths for name, paths in filepaths.items() if len(paths) > 1}
 
 
 def are_files_duplicates(file_path1, file_path_2):
-    pass
+    if os.path.getsize(file_path1) == os.path.getsize(file_path_2):
+        return True
+
+
+def search_duplicates(filepaths):
+    duplicates = []
+    for path_list in filepaths.values():
+        duplicates += [pair for pair in combinations(path_list, 2) if are_files_duplicates(*pair)]
+    return duplicates
 
 
 if __name__ == '__main__':
-    pass
+    folder_content = inspect_folder(sys.argv[1])
+    if folder_content is None:
+        print("Не удалось прочитать папку")
+    else:
+        duplicates = search_duplicates(folder_content)
+        for pair in duplicates:
+            print('Дубликаты: {} и {}'.format(pair[0], pair[1]))
